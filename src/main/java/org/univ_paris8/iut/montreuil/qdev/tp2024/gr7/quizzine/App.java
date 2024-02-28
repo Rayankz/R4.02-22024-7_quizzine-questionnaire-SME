1c634ce2fc9cf40442b095f1b570f67c7847cb0d
vithu <vvijayatharan@iut.univ-paris8.fr>
2024-02-28 17:15:18 +0100

    Initial commit

---
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr7/quizzine/App.java
@@ -0,0 +1,13 @@
+package org.univ_paris8.iut.montreuil.qdev.tp2024.gr7.quizzine;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
