327be23a8e354dfbeb30165cc8cd4a77d56ce445
VVithushan <vvijayatharan@iut.univ-paris8.fr>
2024-03-13 15:30:24 +0100

    1er push

---
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr7/quizzine/entities/bo/QuestionnairesBO.java
@@ -0,0 +1,4 @@
+package org.univ_paris8.iut.montreuil.qdev.tp2024.gr7.quizzine.entities.bo;
+
+public class QuestionnairesBO {
+}
---
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr7/quizzine/utiles/PasBonFichierException.java
@@ -0,0 +1,4 @@
+package org.univ_paris8.iut.montreuil.qdev.tp2024.gr7.quizzine.utiles;
+
+public class PasBonFichierException extends ChargerCSVException{
+}
